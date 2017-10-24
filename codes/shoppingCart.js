a86979899af40849a51d744f0cbfda76eb13f45c
tunmise <ogunniyitunmise@gmail.com>
2017-10-24 15:54:41 +0100

    Create ShoppingCart class with only the constructor

---
+++ codes/shoppingCart.js
@@ -0,0 +1,8 @@
+// This file contains all the shoppingCart software's code.class ShoppingCart 
+
+class ShoppingCart {
+    constructor(){
+        this.total = 0;
+        this.items = {};
+    }
+}
