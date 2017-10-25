0303cc7481b5547d698d2baa7338c977bf9eb211
tunmise <ogunniyitunmise@gmail.com>
2017-10-25 06:21:20 +0100

    Remove comments in between codes

--- codes/promoShoppingCart.js
+++ codes/promoShoppingCart.js
@@ -1,22 +1,16 @@
-// Import the classes from shoppingCart
 let shop = require('./shoppingCart');
 
-// Retrieve the two classes created in the shoppingCart file.
 let ShoppingCart = shop.ShoppingCart;
 let Item = shop.Item;
 
 class PromoShoppingCart extends ShoppingCart {
-    // This class is built on the shoppingCart class.
-    // It is gives the user a promo usage by allowing him buy 20 items without billing.
     constructor() {
-        // initialize property _promoValue to track how long the promo last.
         super();
         this._promoValue = 20;
     }
 
     
     addItem(name, quantity, price, discount) {
-        // Allow adding 20 item without billing the user.
         let item = new Item(name, price, discount); 
         this._promoValue --;
         if(this._promoValue >= 0) {
@@ -28,7 +22,6 @@ class PromoShoppingCart extends ShoppingCart {
     }
 
     removeItem(name, quantity, price, discount) {
-        // Just like the method it overides, but takes care of if the user has a promoValue.
         let item = new Item(name, price, discount);
         this._promoValue ++;
         if(this._items[item.name]) {
@@ -40,7 +33,6 @@ class PromoShoppingCart extends ShoppingCart {
     }
 
     get promoValue() {
-        // Output the value of the promovalue
         return this._promoValue;
     } 
 
--- codes/shoppingCart.js
+++ codes/shoppingCart.js
@@ -1,15 +1,11 @@
 class ShoppingCart {
-    // A shopping cart class;
     constructor(){
-        // Initialize properties, _total to track price, 
-        // _items to track items and _itemNumber to track number of items.
         this._total = 0;
         this._items = {};
         this._itemNumber = 0;
     }
 
     addItem(name, quantity, price, discount) {
-        // This method adds items to the cart and take care of if the item has a discount.
         let item = new Item(name, price, discount); 
         this._total += quantity * item.price;
         this._items[item.name] = quantity;
@@ -17,7 +13,6 @@ class ShoppingCart {
     }
 
     removeItem(name, quantity, price, discount) {
-        // This method remmoves items from the cart and takes care of if the item has a discount
         let item = new Item(name, price, discount);
         if(this._items[item.name]) {
             this._total -= item.price * quantity;
@@ -29,7 +24,6 @@ class ShoppingCart {
     }
 
     checkout(payment) {
-        // This method handles the user payment.
         if(payment < this._total) {
            return `You need extra ${this._total - payment} to checkout`;
         } else {
@@ -41,7 +35,6 @@ class ShoppingCart {
     }
 
     viewItems() {
-        // This method displays all the user activity in the console.
         console.log(`Items \t\t Amount`)
         for(let item in this._items) {
             console.log(`${item} \t\t ${this._items[item]}`)
@@ -50,33 +43,26 @@ class ShoppingCart {
     }
 
     get total() {
-        // Outputs the value of total
         return this._total;
     }
 
     get itemNumber() {
-        // Outputs the value of total
         return this._itemNumber;
     }
  }
 
 class Item {
-    // This class is used to instantiate an item.
     constructor(name, price, discount) {
-        // Initialize properties, _name for the item name, 
-        // _price for the item price, _discount for the item discount 
         this._name = name;
         this._price = price;
         this._discount = discount; 
     }
     
     get name() {
-        // Outputs the name of the item.
         return this._name;
     }
 
     get price() {
-        // Outputs the price of the item and take care of if the item has a discount.
         if(this._discount === undefined) {
             return this._price;
         } else {
